22b29acf8ddbba19fa568e883f31e56b18036286
yandex-zsh <airtatzolmillo@gmail.com>
2025-09-26 13:39:39 +0800

    merge

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 
 import React from "react";
+import CounterApp from './CounterApp';
+import ColorChangerApp from './ColorChangerApp';
 import ChatScreen from './ChatScreen';
 import CommentSection from "./CommentSection";
 
 export default function App() {
   return (
         <>
+          <CounterApp />
+          <ColorChangerApp />
           <ChatScreen />
           <CommentSection />
         </>
